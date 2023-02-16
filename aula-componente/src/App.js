6ca43484724ffa88155cb23fcfc406892d9fe945
Jésus Baro <118184872+JesusBaro@users.noreply.github.com>
2023-02-15 23:01:39 -0300

    enviando aula 1

---
+++ aula-componente/src/App.js
@@ -0,0 +1,67 @@
+//PASSO A PASSO PARA INICIAR UM PROJETO EM REACT
+// 1º  npx create-react-app nome-do-projeto
+// 2º  cd nome-do-projeto
+// 3º  abrir o vs code
+// 4º  npm start  
+
+// 5º (Não esqueça de parar o projeto ao terminar com ctrl + c)
+
+// JSX = nos permite mesclar o js com elementos html. mescla html e javascript.
+//
+
+
+
+// componete funcional - sem mudana de estado
+
+// import React from 'react'; //importanto a bliblioteca do react
+//sintax
+// function Teste() {
+//   return(
+//     <div>
+// <h1>`olá mundo`</h1>
+
+//     </div>
+// )
+
+// }
+
+// export default Teste;
+
+
+
+//componente de class
+//sintax
+//Class é uma palavra reservada do JS para criação de componentes inteligentes
+//
+// import React, {component} from 'react';
+
+// class nome extends component{
+//   render (){  // método de manipulação do componete
+//     return( //funciona para mais de uma oconteudo 
+// <div>Primeira aula</div>
+// // <> </>  - fragmento
+//     )
+//   }
+// }
+
+// export default nome;
+
+// revisão estrutrura de componente de class
+
+// import React, { Component } from 'react'; //importanto biblioteca  react
+
+// class App extends Component { 
+// //o class app extende ao componente.
+
+//   render(){ 
+// //método de manipulação do componete e renderiza a classe na página. 
+//     return(
+// //return 
+
+// <div> boa noite </div> // pra colocar mais de 1 item
+//     )
+//   }
+// }
+
+// export default App;
+
